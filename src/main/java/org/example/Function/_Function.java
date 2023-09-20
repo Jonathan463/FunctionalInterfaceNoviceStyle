805784ef1ecc4449d8db6c664cdffc49ee561f74
Jonathan463 <godsonjonathan39@gmail.com>
2023-09-20 17:29:59 -0500

    Chaining Function with AndThen

--- src/main/java/org/example/Function/_Function.java
+++ src/main/java/org/example/Function/_Function.java
@@ -4,10 +4,20 @@
 
  public class _Function {
     public static void main(String[] args) {
-        System.out.println(increment(2));
-        Integer result = incrementByOne.apply(1);
+        System.out.println(increment(200));
+        Integer result = incrementByOne.apply(200 );
+
+        Integer multiply = multiplyBy10.apply(result);
+        System.out.println(multiply);
 
         System.out.println(result);
+
+        System.out.println("Length of result "+numberToString.apply(result));
+
+        //Chaining result with andThen
+        Function<Integer, String> multiplyBy10AndThenToString = multiplyBy10.andThen(numberToString);
+        System.out.println("AndThen Value "+multiplyBy10AndThenToString.apply(3));
+
     }
     //Imperative approach
     static int increment(int number){
@@ -16,4 +26,8 @@
 
     //Declarative approach
     static Function<Integer, Integer> incrementByOne = number -> number + 1;
+
+    static Function<Integer, String> numberToString = number -> String.valueOf(number.toString().length());
+
+    static Function<Integer,Integer> multiplyBy10 = number -> number * 10;
 }
